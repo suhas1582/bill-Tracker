c652c2f4c51ca06af41868428b9a23e791823fe9
Suhas R N <uzumakinaruto1582@gmail.com>
2020-09-19 19:18:57 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bill Tracker',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bill Tracker'),
+      ),
+      body: Center(
+        child: Text(
+          'Hello World!',
+        ),
+      ),
+    );
+  }
+}
